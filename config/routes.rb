4ef8eea752e28cab27cd521acac3165c0e9c79b2
Artur Rodkin <das.modal@gmail.com>
2022-10-11 21:15:08 +0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :shops, except: %i[destroy] do
+        post :buy, on: :member
+      end
+      resources :users, except: %i[destroy]
+      resources :cards, only: %i[index show]
+    end
+  end
 end
