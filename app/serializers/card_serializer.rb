2686703713d063b0d7807f03d4f3389a475b00d7
Artur Rodkin <das.modal@gmail.com>
2022-10-12 22:37:18 +0700

    add relationships to CardSerializer

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -2,4 +2,14 @@ class CardSerializer < ActiveModel::Serializer
   type :cards
 
   attributes :id, :bonuses
+  
+  has_one :user do
+    include_data false
+    link(:related) { api_v1_user_path(id: object.id) }
+  end
+
+  has_one :shop do
+    include_data false
+    link(:related) { api_v1_shop_path(id: object.id) }
+  end
 end
