3f16c1efecfb8a393ad3d1b425eb3372fabd47c9
Artur Rodkin <das.modal@gmail.com>
2022-10-12 13:56:08 +0700

    initial fill index, show, create, update actions in ShopsController

--- app/controllers/api/v1/shops_controller.rb
+++ app/controllers/api/v1/shops_controller.rb
@@ -2,15 +2,31 @@ module Api
   module V1
     class ShopsController < ApplicationController
       def index
+        shops = Shop.all
+
+        render json: shops, status: :ok
       end
 
       def show
+        shop = Shop.find(params[:id])
+
+        render json: shop, status: :ok
       end
 
       def create
+        shop = Shop.new(name: params[:name])
+
+        if shop.save
+          render json: shop, status: :created
+        end
       end
 
       def update
+        shop = Shop.find(params[:id])
+
+        if shop.update(name: params[:name])
+          render json: shop, status: :ok
+        end
       end
 
       def buy
