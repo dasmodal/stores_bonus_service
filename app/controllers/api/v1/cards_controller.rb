17a4c53786ca627373dc96f30d95753245366896
Artur Rodkin <das.modal@gmail.com>
2022-10-12 18:23:48 +0700

    fill actions in Cards Controller

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -2,9 +2,15 @@ module Api
   module V1
     class CardsController < ApplicationController
       def index
+        cards = Card.all
+
+        render json: cards, status: :ok
       end
 
       def show
+        card = Card.find(params[:id])
+
+        render json: card, status: :ok
       end
     end
   end
