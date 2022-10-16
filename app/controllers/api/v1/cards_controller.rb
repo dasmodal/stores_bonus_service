16ab5f72ab1f71ab2ec43472f7a6ed42209834e8
Artur Rodkin <das.modal@gmail.com>
2022-10-16 20:31:15 +0700

    add bonuses_sum to card controller

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -7,13 +7,25 @@ module Api
         cards = Card.by_user(params.dig(:filter, :user_id)) if params.dig(:filter, :user_id)
         cards = Card.by_shop(params.dig(:filter, :shop_id)) if params.dig(:filter, :shop_id)
 
-        render json: cards, status: :ok
+        meta = bonuses_sum(cards)
+
+        render json: cards, status: :ok, meta: meta
       end
 
       def show
         card = Card.find(params[:id])
 
         render json: card, status: :ok
+      end
+
+      private
+
+      def bonuses_sum(cards)
+        if params.dig(:stats, :bonuses) == 'sum'
+          sum = cards.pluck(:bonuses).sum
+
+          { "stats": { "bonuses": { "sum": sum } } }
+        end
       end
     end
   end
