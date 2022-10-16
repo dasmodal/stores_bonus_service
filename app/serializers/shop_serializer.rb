bc07f2fa864ea35abd74e2c79e31aa0c4441a39a
Artur Rodkin <das.modal@gmail.com>
2022-10-16 12:26:24 +0700

    add relations to user and shop serializers

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -2,4 +2,14 @@ class ShopSerializer < ActiveModel::Serializer
   type :shops
 
   attributes :id, :name
+
+  has_many :cards do
+    include_data false
+    link(:related) { "#{api_v1_cards_path}?filter[shop_id]=#{object.id}" }
+  end
+
+  has_many :users do
+    include_data false
+    link(:related) { "#{api_v1_users_path}?filter[shop_id]=#{object.id}" }
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,14 @@ class UserSerializer < ActiveModel::Serializer
   type :users
 
   attributes :id, :email, :negative_balance
+
+  has_many :cards do
+    include_data false
+    link(:related) { "#{api_v1_cards_path}?filter[user_id]=#{object.id}" }
+  end
+
+  has_many :shops do
+    include_data false
+    link(:related) { "#{api_v1_shops_path}?filter[user_id]=#{object.id}" }
+  end
 end
